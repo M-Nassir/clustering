94b17cbf7b08996ad386a6843ef6b82290312808
M-Nassir <Nassir.M@live.co.uk>
2025-06-23 15:02:43 +0100

    script for setting up package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clustering-nassir",  # PyPI name (can have dashes)
+    version="0.1.0",
+    author="Nassir Mohammad",
+    author_email="drnassirmohammad@gmail.com",
+    description="A semi-supervised clustering method using anomaly detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-nassir/clustering",  # update this
+    packages=setuptools.find_packages(include=["clustering_nassir", "clustering_nassir.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    license="MIT",
+)
