098f417b4ac96bc685a8396b32ee9faedc441cb1
M-Nassir <Nassir.M@live.co.uk>
2025-07-22 13:46:36 +0100

    update of guides and build for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="clustering-nassir",  # PyPI name (can have dashes)
-    version="0.1.0",
+    version="0.1.1",
     author="Nassir Mohammad",
     author_email="drnassirmohammad@gmail.com",
     description="A semi-supervised clustering method using anomaly detection",
